c48d0d79c474a926610cba5ce6c4436c1a87d034
averbovs <andrei_verbovskii@epam.com>
2017-07-02 16:01:24 +0300

    pol

--- police/js/police.js
+++ police/js/police.js
@@ -65,97 +65,17 @@
             if (window.ymaps) {
                 ymaps.ready(createMap);
             } else {
-                createStatic()
+                map = createStatic();
+                initArgs.map = map;
+                Core.trigger('map-init', initArgs)
+                Core.trigger('map-ready', initArgs)
+                renderMainList();
+                loading(false)
             }
         })
 
-        function createStatic() {
-            var $map = $('#map'),
-                dpoints = [],
-                dtimeout;
-            $map.on('click', function() {
-                $('body').addClass('mobile-details-view')
-            })
-            map = {
-                setCenter: function(c, zoom) {
-                    if (!zoom) zoom = 10;
-                    this.render(c, zoom)
-                },
-                delayMarkPoint: function(p, zoom) {
-                    return; //!!!!
-                    dpoints.push(p)
-                    if (dtimeout) return;
-                    dtimeout = setTimeout(function() {
-                        var sum = [0, 0]
-                        dpoints.forEach(function(p) {
-                            sum[0] += p.coords[0];
-                            sum[1] += p.coords[1];
-                        })
-                        var c = [sum[0] / dpoints.length, sum[1] / dpoints.length];
-                        //console.log('ce', c)
-                        map.markPoints(c, dpoints, zoom)
-                        console.log('delayMarkPoint', dpoints)
-                        dpoints = [];
-                        dtimeout = null;
-                    }, 300)
-                },
-                markPoint: function(p, zoom) {
-                    console.warn('markPoint', p);
-                    this.render(p.coords, zoom, [p])
-                },
-                markPoints: function(c, points, zoom) {
-                    console.warn('markPoints', c, points);
-                    this.render(c, zoom, points)
-                },
-                render: function(c, zoom, points) {
-                    console.warn('render', c, points);
-                    var url = 'https://static-maps.yandex.ru/1.x/?ll={0},{1}&l=map&';
-                    var pt = '';
-                    if (points) {
-                        if (points.length == 1) zoom = 15;
-                        pt = '&pt=';
-                        points.forEach(function(p) {
-                            var pc = p.coords;
-                            pt += '{0},{1},{2}~'.format(pc[1], pc[0], p.preset);
-                        })
-                        pt = pt.substr(0, pt.length - 1)
-                    }
-                    if (zoom) {
-                        url += 'z={0}&'.format(zoom)
-                    }
-                    $map.css('background-image', 'url({0})'.format(url.format(c[1], c[0], zoom) + pt))
-                }
-            };
-            initArgs.map = map;
-            Core.trigger('map-init', initArgs)
-            Core.trigger('map-ready', initArgs)
-            renderMainList();
-            loading(false)
-        }
-
-        function renderStaticHome(c) {
-            return;
-            var rp = regions.filter(function(r) {
-                return r.region.point
-            }).map(function(r) {
-                return {
-                    coords: r.region.point.coords,
-                    preset: 'pmlbs' + r.region.number
-                }
-            })
-            var dp = departments.filter(function(d) {
-                    return d.department.coords
-                }).map(function(d) {
-                    return {
-                        coords: d.department.coords,
-                        preset: 'pmbls'
-                    }
-                }) //    .slice(0,5)
-            map.markPoints(c, dp.concat(rp))
-        }
-
         function createMap(state) {
-            map = new ymaps.Map('map', { controls: ["zoomControl"], zoom: 12, center: [59.948814, 30.309640]}, {suppressMapOpenBlock: true});
+            map = new ymaps.Map('map', { controls: ["zoomControl"], zoom: 12, center: [59.948814, 30.309640] }, { suppressMapOpenBlock: true });
             initArgs.map = map;
             Core.trigger('map-init', initArgs)
             renderMainList();
@@ -282,27 +202,6 @@
                 })
             })
         }
-        Core.on('map.click', function(args) {
-            resolvePoint(args.coords)
-        })
-
-        function searchPoint(pos) {
-            console.log('searchPoint', pos)
-            if (!map) return;
-            for (var i = 0; i < regions.length; i++) {
-                var r = regions[i];
-                if (r.pol && r.pol.geometry.contains(pos)) {
-                    r.render()
-                    return;
-                }
-            }
-        }
-        $('#btn-locate').on('click', function() {
-            location(function(p) {
-                searchPoint(p)
-                resolvePoint(p)
-            })
-        })
         Core.on('map-ready', function() {
             location(function(p) {
                 var nearest;
@@ -319,7 +218,7 @@
                 $cities.eq(0).trigger('click')
             })
         })
-       
+
         function location(success, error) {
             loading(true)
             navigator.geolocation.getCurrentPosition(function(location) {
@@ -332,142 +231,11 @@
                 if (error) error()
             })
         }
-        Core.on('map.set-center', function() {
-        })
-        var curTimeout;
+        Core.on('map.set-center', function() {})
 
-        // var cp;
         function markCurrent(addr) {
-
-            // if (window.ymaps) {
-            //     if (cp) map.geoObjects.remove(cp);
-            //     if (!p) return;
-            //     map.setCenter(p)
-            //     cp = new ymaps.Placemark(p, {
-            //         iconCaption: addr || '...',
-            //         hintContent: addr || '...'
-            //     }, {
-            //         preset: 'islands#redCircleDotIconWithCaption',
-            //         iconColor: '#f00'
-            //     });
-            //     clearTimeout(curTimeout)
-            //     curTimeout = setTimeout(function() {
-            //         if (cp) map.geoObjects.remove(cp);
-            //     }, 3000)
-            //     map.geoObjects.add(cp);
-            // } else if (p) {
-            //     map.markPoint({ coords: p, preset: 'flag' })
-            // }
-            Core.trigger('map-click.resolved', { addr : addr});
+            Core.trigger('map-click.resolved', { addr: addr });
         }
-
-        function resolvePoint(p) {
-            markCurrent()
-            API.resolvePoint(city, p, function(addr) {
-                if (!addr[0]) return;
-                var name = addr[0].name;
-                //if (cp) cp.properties.set('iconCaption', name).set('hintContent', name);
-                var pq = parseQuery(name);
-                markCurrent(name);
-                var strres = search(streets, pq, function(o) {
-                        if (o) return o.name
-                    })
-                    // console.log(addr[0].name, strres[0])
-                if (strres[0]) {
-                    var sec = strres[0].item.sector;
-                    sec.select(false, true)
-                }
-            })
-        }
-        var $txtSearch = $('#txt-search')
-            .on('focus', function() { this.select() })
-            .on('change', function(e, args) {
-                if (args) {
-                    markCurrent()
-                    var $row = args.$row,
-                        dsind = Number($row.attr('data-dsindex')),
-                        ds = args.data.filter(function(d) { return d.dsindex == dsind })[0],
-                        ind = $row.index(),
-                        o = ds.data[ind].item;
-                    if (dsind == 0) { //yandex addr
-                        map.setCenter(o.coords)
-                        markCurrent(o.name, o.coords)
-                    } else if (dsind == 2) { //sector streets
-                        o.sector.select(true)
-                        API.resolveAddr(city, o.name, function(data) {
-                            var d = data[0];
-                            if (d) markCurrent(d.name, d.coords)
-                        })
-                    } else if (dsind == 1) { //region
-                        o.select(true)
-                    } else if (dsind == 3) { //sector
-                        o.select(true)
-                    } else if (dsind == 4) { //person
-                        o.location.select(true)
-                    } else if (dsind == 5) { //person
-                        o.select(true)
-                    }
-                }
-            })
-
-        function search(arr, ws, fname) {
-            var res = []
-            arr.forEach(function(o) {
-                var matches = [],
-                    rate = 0;
-                var s = fname(o);
-                if (!s) return;
-                var slc = s.toLowerCase();
-                ws.forEach(function(w) {
-                    var ind = slc.indexOf(w);
-                    if (ind >= 0) matches.push({ w: w, ind: ind })
-                    if (ind == 0) rate++;
-                })
-                if (matches.length > 0) {
-                    rate += matches.length;
-                    res.push({ name: s, matches: matches, rate: rate, item: o })
-                }
-            })
-            res.sort(function(a, b) {
-                    return b.rate - a.rate
-                })
-                //console.warn(res);
-            return res.slice(0, 5);
-        }
-        $txtSearch.autocomplete($('#search-popup'), templates.autocomplete, function(q, success) {
-            if (!q) return;
-            var pq = parseQuery(q);
-            var regres = search(regions, pq, function(o) {
-                return o.region.name
-            })
-            var depres = search(departments, pq, function(o) {
-                return o.department.name
-            })
-            var secres = search(sectors, pq, function(o) {
-                return o.sector.name
-            })
-            var strres = search(streets, pq, function(o) {
-                if (o) return o.name
-            })
-            var perres = search(persons, pq, function(o) {
-                if (o) return o.name
-            })
-            var yres = []
-            var res = [
-                //{ title: 'Карта', type: 'map', dsindex: 0, data: yres },
-                { title: 'Отделения', type: 'regions', dsindex: 1, data: regres },
-                { title: 'Адрес', type: 'addrs', dsindex: 2, data: strres },
-                { title: 'Участковые', type: 'sectors', dsindex: 3, data: secres },
-                { title: 'Начальники', type: 'persons', dsindex: 4, data: perres },
-                { title: 'ОУМВД', type: 'departments', dsindex: 5, data: depres },
-            ]
-            success(res)
-                //console.log(res)
-            return API.resolveAddr(city, q, function(data) {
-                data.forEach(function(d) { yres.push({ name: d.name, item: d }) })
-                success(res)
-            })
-        })
 
         function checkState() {
             if (state && state.rowId >= 0 && state.type) {
@@ -486,18 +254,6 @@
             state = args.state;
             checkState()
         })
-
-        function parseQuery(q) {
-            var pq = q.toLowerCase().split(/[\s,]+/);
-            var numbers = pq.map(function(s) {
-                    return parseInt(s)
-                }) //.filter(function(s) { return !!s})
-            pq.forEach(function(p, i) {
-                    if (numbers[i]) pq[i] = (numbers[i]);
-                })
-                //return pq.concat(numbers)
-            return pq;
-        }
 
         function yresolve(q, success) {
             API.resolveAddr(city, q, function(addrs) {
@@ -520,3 +276,22 @@
             //console.log(getcolors())
     };
 })()
+// if (window.ymaps) {
+//     if (cp) map.geoObjects.remove(cp);
+//     if (!p) return;
+//     map.setCenter(p)
+//     cp = new ymaps.Placemark(p, {
+//         iconCaption: addr || '...',
+//         hintContent: addr || '...'
+//     }, {
+//         preset: 'islands#redCircleDotIconWithCaption',
+//         iconColor: '#f00'
+//     });
+//     clearTimeout(curTimeout)
+//     curTimeout = setTimeout(function() {
+//         if (cp) map.geoObjects.remove(cp);
+//     }, 3000)
+//     map.geoObjects.add(cp);
+// } else if (p) {
+//     map.markPoint({ coords: p, preset: 'flag' })
+// }
---
+++ police/js/static-map.js
@@ -0,0 +1,81 @@
+'use strict';
+function createStatic() {
+    var $map = $('#map'),
+        dpoints = [],
+        dtimeout;
+    $map.on('click', function() {
+        $('body').addClass('mobile-details-view')
+    })
+    var map = {
+        setCenter: function(c, zoom) {
+            if (!zoom) zoom = 10;
+            this.render(c, zoom)
+        },
+        delayMarkPoint: function(p, zoom) {
+            return; //!!!!
+            dpoints.push(p)
+            if (dtimeout) return;
+            dtimeout = setTimeout(function() {
+                var sum = [0, 0]
+                dpoints.forEach(function(p) {
+                    sum[0] += p.coords[0];
+                    sum[1] += p.coords[1];
+                })
+                var c = [sum[0] / dpoints.length, sum[1] / dpoints.length];
+                //console.log('ce', c)
+                map.markPoints(c, dpoints, zoom)
+                console.log('delayMarkPoint', dpoints)
+                dpoints = [];
+                dtimeout = null;
+            }, 300)
+        },
+        markPoint: function(p, zoom) {
+            console.warn('markPoint', p);
+            this.render(p.coords, zoom, [p])
+        },
+        markPoints: function(c, points, zoom) {
+            console.warn('markPoints', c, points);
+            this.render(c, zoom, points)
+        },
+        render: function(c, zoom, points) {
+            console.warn('render', c, points);
+            var url = 'https://static-maps.yandex.ru/1.x/?ll={0},{1}&l=map&';
+            var pt = '';
+            if (points) {
+                if (points.length == 1) zoom = 15;
+                pt = '&pt=';
+                points.forEach(function(p) {
+                    var pc = p.coords;
+                    pt += '{0},{1},{2}~'.format(pc[1], pc[0], p.preset);
+                })
+                pt = pt.substr(0, pt.length - 1)
+            }
+            if (zoom) {
+                url += 'z={0}&'.format(zoom)
+            }
+            $map.css('background-image', 'url({0})'.format(url.format(c[1], c[0], zoom) + pt))
+        }
+    };
+    return map;
+}
+
+function renderStaticHome(c) {
+    return;
+    var rp = regions.filter(function(r) {
+        return r.region.point
+    }).map(function(r) {
+        return {
+            coords: r.region.point.coords,
+            preset: 'pmlbs' + r.region.number
+        }
+    })
+    var dp = departments.filter(function(d) {
+            return d.department.coords
+        }).map(function(d) {
+            return {
+                coords: d.department.coords,
+                preset: 'pmbls'
+            }
+        }) //    .slice(0,5)
+    map.markPoints(c, dp.concat(rp))
+}
