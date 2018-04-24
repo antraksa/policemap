698081c5d8fead3d90c1272b52017e1df20b4086
antraksa <antraksamail@gmail.com>
2018-04-24 19:26:26 +0300

    some corrections

--- police/php/menu.php
+++ police/php/menu.php
@@ -2,5 +2,5 @@
   <a href=".">Главная</a>
   <a href="about.php">О проекте</a>
   <a href="how-to-use.php">Как пользоваться</a>
-  <a href="http://policemap.tilda.ws/documents" target="_blank">FAQ о полиции</a>
+  <a href="http://policemap.tilda.ws/" target="_blank">FAQ о полиции</a>
 </nav>
